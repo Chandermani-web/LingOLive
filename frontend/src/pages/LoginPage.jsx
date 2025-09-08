fc723bbedb04aada61dfac8f01c15662a9b8dd2c
chandermanimishra91@gmail.com <chandermanimishra91.in>
2025-09-08 19:48:41 +0530

    Complete Projects

---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,174 @@
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import useLogin from "../hooks/useLogin";
+import "remixicon/fonts/remixicon.css";
+
+const LoginPage = () => {
+  const [showpassword, setshowpassword] = useState(false);
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: loginMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: login,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  // This is how we did it using our custom hook - optimized version
+  const { isPending, error, loginMutation } = useLogin();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    loginMutation(loginData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* LOGIN FORM SECTION */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col justify-center">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <ShipWheelIcon className="size-9 text-amber-500" />
+            <span className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-blue-700 tracking-wider">
+              LingOLive
+            </span>
+          </div>
+
+          {/* ERROR MESSAGE DISPLAY */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+          <div className="w-full">
+            <form onSubmit={handleLogin}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Welcome Back</h2>
+                  <p className="text-sm opacity-70">
+                    Sign in to your account to continue your language journey
+                  </p>
+                </div>
+
+                <div className="flex flex-col gap-3">
+                  <div className="form-control w-full space-y-2">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="hello@example.com"
+                      className="border-2 border-zinc-400 px-3 py-2 w-full outline-0 rounded-xl placeholder:capitalize placeholder:text-zinc-400 focus-within:border-blue-400 focus-within:shadow-2xs focus-within:shadow-blue-400"
+                      value={loginData.email}
+                      onChange={(e) =>
+                        setLoginData({ ...loginData, email: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+
+                  <div className="form-control w-full space-y-2 relative">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type={showpassword ? "text" : "password"}
+                      placeholder="••••••••"
+                      className="border-2 border-zinc-400 px-3 py-2 w-full outline-0 rounded-xl placeholder:capitalize placeholder:text-zinc-400 focus-within:border-blue-400 focus-within:shadow-2xs focus-within:shadow-blue-400"
+                      value={loginData.password}
+                      onChange={(e) =>
+                        setLoginData({ ...loginData, password: e.target.value })
+                      }
+                      required
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-5 top-8 text-xl"
+                      onClick={() => {
+                        setshowpassword(!showpassword);
+                      }}
+                    >
+                      {showpassword ? (
+                        <i className="ri-eye-off-line"></i>
+                      ) : (
+                        <i className="ri-eye-line"></i>
+                      )}
+                    </button>
+                  </div>
+
+                  <button
+                    type="submit"
+                    className="font-semibold bg-blue-600 py-2 rounded-2xl hover:-translate-y-1 hover:bg-green-500 hover:shadow-2xl hover:shadow-green-300 transition-all duration-300"
+                    disabled={isPending}
+                  >
+                    {isPending ? (
+                      <>
+                        <span className="loading loading-spinner loading-xs"></span>
+                        Signing in...
+                      </>
+                    ) : (
+                      "Sign In"
+                    )}
+                  </button>
+
+                  <div className="text-center mt-4">
+                    <p className="text-sm">
+                      Don't have an account?{" "}
+                      <Link
+                        to="/signup"
+                        className="text-blue-500 hover:underline"
+                      >
+                        Create one
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* IMAGE SECTION */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="../../public/pexels-pixabay-41949.jpg"
+                alt="Language connection illustration"
+                className="w-full h-full object-cover absolute"
+              />
+              <h1 className="absolute text-5xl h-full w-full text-center place-content-center opacity-30">
+                Lingolive
+              </h1>
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="text-xs opacity-70">
+                Connect with people across the globe and break down language
+                barriers to communicate freely and confidently.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,215 @@
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+
+import useSignUp from "../hooks/useSignUp";
+
+const SignUpPage = () => {
+  const [showpassword, setshowpassword] = useState(false);
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: signupMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  // This is how we did it using our custom hook - optimized version
+  const { isPending, error, signupMutation } = useSignUp();
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    signupMutation(signupData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-start p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* SIGNUP FORM - LEFT SIDE */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col justify-center">
+          {/* LOGO */}
+          <div className="mb-4 flex items-center gap-2">
+            <ShipWheelIcon className="size-9 text-amber-500" />
+            <span className="text-3xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-blue-800 tracking-wider">
+              LingOLive
+            </span>
+          </div>
+
+          {/* ERROR MESSAGE IF ANY */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p className="text-sm opacity-70">
+                    Join Streamify and start your language learning adventure!
+                  </p>
+                </div>
+
+                <div className="space-y-3">
+                  {/* FULLNAME */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="John Doe"
+                      className="border-2 border-zinc-400 px-3 py-2 w-full outline-0 rounded-xl placeholder:capitalize placeholder:text-zinc-400 focus-within:border-blue-400 focus-within:shadow-2xs focus-within:shadow-blue-400"
+                      value={signupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+                  {/* EMAIL */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="john@gmail.com"
+                      className="border-2 border-zinc-400 px-3 py-2 w-full outline-0 rounded-xl placeholder:capitalize placeholder:text-zinc-400 focus-within:border-blue-400 focus-within:shadow-2xs focus-within:shadow-blue-400"
+                      value={signupData.email}
+                      onChange={(e) =>
+                        setSignupData({ ...signupData, email: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+                  {/* PASSWORD */}
+                  <div className="form-control w-full relative">
+                    <label className="label">
+                      <span className="label-text">Password</span>
+                    </label>
+                    <input
+                      type={showpassword ? "text" : "password"}
+                      placeholder="********"
+                      className="border-2 border-zinc-400 px-3 py-2 w-full outline-0 rounded-xl placeholder:capitalize placeholder:text-zinc-400 focus-within:border-blue-400 focus-within:shadow-2xs focus-within:shadow-blue-400"
+                      value={signupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...signupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                    <p className="text-xs opacity-70 mt-1">
+                      Password must be at least 6 characters long
+                    </p>
+                    <button
+                      type="button"
+                      className="absolute right-5 top-7 text-xl"
+                      onClick={() => {
+                        setshowpassword(!showpassword);
+                      }}
+                    >
+                      {showpassword ? (
+                        <i className="ri-eye-off-line"></i>
+                      ) : (
+                        <i className="ri-eye-line"></i>
+                      )}
+                    </button>
+                  </div>
+
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        I agree to the{" "}
+                        <span className="text-primary hover:underline">
+                          terms of service
+                        </span>{" "}
+                        and{" "}
+                        <span className="text-primary hover:underline">
+                          privacy policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+                <button className="font-semibold bg-blue-600 w-full py-2 rounded-2xl hover:-translate-y-1 hover:bg-green-500 hover:shadow-2xl hover:shadow-green-300 transition-all duration-300" type="submit">
+                  {isPending ? (
+                    <>
+                      <span className="loading loading-spinner loading-xs"></span>
+                      Loading...
+                    </>
+                  ) : (
+                    "Create Account"
+                  )}
+                </button>
+
+                <div className="text-center mt-4">
+                  <p className="text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-500 hover:underline">
+                      Sign in
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* SIGNUP FORM - RIGHT SIDE */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="../../public/pexels-pixabay-41949.jpg"
+                alt="Language connection illustration"
+                className="w-full h-full object-cover absolute"
+              />
+              <h1 className="absolute text-5xl h-full w-full text-center place-content-center opacity-30">
+                Lingolive
+              </h1>
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="opacity-70 text-xs">
+                Connect with people across the globe and break down language
+                barriers to communicate freely and confidently.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
